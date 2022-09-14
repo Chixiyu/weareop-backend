18e13b9172d2433771e9b124ce7d59135a0815c4
Chixiyu <2980364872@qq.com>
2022-09-14 12:21:52 +0800

    Added login interceptor

---
+++ src/main/java/com/weareop/weareopbackend/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.weareop.weareopbackend.config;
+
+import com.weareop.weareopbackend.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    private LoginInterceptor loginInterceptor;
+
+//     add login interceptor (check login for backstage)
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login")
+                .excludePathPatterns("/logout")
+                .excludePathPatterns("/redis/**");
+    }
+}
---
+++ src/main/java/com/weareop/weareopbackend/interceptor/LoginInterceptor.java
@@ -0,0 +1,48 @@
+package com.weareop.weareopbackend.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
+
+    private static final Logger LOG= LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        LOG.info("mvc Interceptor: {}",request.getHeader("token"));
+        if(request.getMethod().equalsIgnoreCase("OPTIONS")){
+            LOG.info("Returned OPTIONS");
+            return true;
+        }
+        String path=request.getRequestURL().toString();
+        LOG.info("Login interface intercepted, path: {}",path);
+
+        String token=request.getHeader("token");
+        LOG.info("Starting login verification: token{}",token);
+        if(token==null||token.isEmpty()){
+            LOG.info("Token is empty, request denied");
+            return false;
+        }
+        String s= redisTemplate.opsForValue().get(token);
+        if(s==null||s.isEmpty()){
+            LOG.warn("Token is invalid, request denied");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());// 401
+            return false;
+        }
+        else{
+            LOG.info("Successfully logged in: {}",s);
+            return true;
+        }
+    }
+}
